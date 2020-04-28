c1a4f6a4f58026117bedee8247b69aa8acd13978
unknown <raees14041995@gmail.com>
2020-04-27 23:30:32 -0700

    'first'

---
+++ blog_project/mysite/blog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from blog.models import Post,comment
+
+
+class PostForm(forms.ModelForm):
+    class Meta():
+        model = Post
+        fields = ('author','title','text')
+
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+class CommentForm(forms.ModelForm):
+    class Meta():
+        model = comment
+        fields = ('author','text')
+
+
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable meddium-editor-textarea'} )
+        }
---
+++ blog_project/mysite/blog/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from django.views.generic import (TemplateView,ListView,CreateView,UpdateView,DeleteView)
+from django.views.generic.detail import DetailView
+from blog.models import Post,comment
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from blog.forms import PostForm,CommentForm
+from django.utils import timezone
+
+# Create your views here.
+
+class AboutView(TemplateView):
+    template_name='about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+      login_url = '/login/'
+      redirect_fields_name = 'blog/post_detail.html'
+      from_class = PostForm
+      model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_fields_name = 'blog/post_detail.html'
+    from_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_fields_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.Objects.filter(published_date__isnull=True).order_by('Create_date')
+####################################################
+####################################################
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_vaild():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form =  CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+
+@login_required
+def comment_approve(request,pk):
+    Comment = get_object_or_404(comment, pk=pk)
+    Comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    Comment = get_object_or_404(comment, pk=pk)
+    post_pk = Comment.post.pk
+    Comment.delete()
+    return redirect('post detail', pk=post_pk)
+
